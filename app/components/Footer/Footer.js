c2f1a8db8b86e2ce689c1ef33c7b314333212370
Marat Gaipov <gaimarat@amazon.com>
2019-08-28 14:30:41 -0700

    Started added

---
+++ app/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.scss';
+
+const Footer = () => (
+  <footer>
+    <section>Made with <span role="img" aria-label="heart-emoji">❤️</span> by Seytech</section>
+  </footer>
+);
+
+export default Footer;
